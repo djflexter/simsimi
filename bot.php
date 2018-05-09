b573f29fdf6ad349afe1a98df928b168227654f0
nonknet <nonknet@gmail.com>
2018-05-09 23:53:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = 'db6a90c1-5e62-46f1-8cd0-7f44f364480a'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
